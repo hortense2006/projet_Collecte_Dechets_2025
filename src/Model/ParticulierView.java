11d33211ef20e9c09f2dbc20fe4ec3a8c19ce1fe
hortense2006 <hortense.galtier@edu.ece.fr>
2025-11-14 17:59:30 +0100

    Update by Hortense n°30

---
+++ src/Model/ParticulierView.java
@@ -0,0 +1,92 @@
+package Model;
+
+import Exceptions.ExceptionPersonnalisable;
+
+import java.util.Map;
+import java.util.Scanner;
+import java.util.UUID;
+
+public class ParticulierView implements Utilisateur
+{
+    // ATTRIBUTS
+    private Map<String, Profil> compte;
+
+    // APPEL DE CLASSES
+    Scanner sc = new Scanner(System.in);
+
+    // METHODE n°1 : Login de l'utilisateur
+    @Override
+    public void login()
+    {
+        String choix;
+        System.out.println("Etes-vous déjà inscrit ?");
+        choix = sc.nextLine();
+        switch(choix)
+        {
+            case "oui":
+            { // Connexion (lecture du fichier texte)
+                signin();
+                break;
+            }
+            case "non":
+            { // Se créer un compte (écriture d'un fichier)
+                register();
+                break;
+            }
+            default:{throw new ExceptionPersonnalisable("Choix invalide.");}
+        }
+    }
+
+    // METHODE n°2 : CONNEXION
+    public void signin()
+    {
+        System.out.println("Saisissez votre identifiant:");
+        String idPropose = sc.nextLine(); // On récupère l'identifiant saisi
+        if(compte.containsKey(idPropose)) // Lecture du fichier : l'identifiant existes
+        {
+            Profil p = compte.get(idPropose);
+            System.out.println("Saisissez votre mot de passe:");
+            String mdpPropose = sc.nextLine(); // On récupère le mdp saisi
+            if(p.getMdp().equals(mdpPropose))// On vérifie si c'est le bon mot de passe
+            {
+                p.setEstConnecte(true);
+                System.out.println("Connexion réussie !");
+            }
+            else
+            {
+                System.out.println("Ce mot de passe est invalide.");
+                signin();
+            }
+        }
+        else
+        {
+            System.out.println("Cet identifiant n'existes pas.");
+            signin();
+        }
+    }
+
+    // METHODE n°3 : INSCRIPTION
+    public void register()
+    {
+        try{}
+        catch(ExceptionPersonnalisable e){}
+        System.out.println("Saisissez votre prenom");
+        String prenom = sc.nextLine();
+        System.out.println("Saisissez votre nom de famille:");
+        String nom = sc.nextLine();
+        System.out.println("Saisissez votre numero d'habitation:");
+        int numero = sc.nextInt();
+        sc.nextLine(); // Vide le buffer
+        System.out.println("Saisissez le nom de votre rue:");
+        String rue = sc.nextLine();
+        String id = UUID.randomUUID().toString(); // Chaque id est différent
+        System.out.println("Voici votre identifiant:" + id);
+        System.out.println("Saisissez un mot de passe:");
+        String mdp = sc.nextLine();
+        // On crée un profil
+        Profil p = new Profil(prenom,nom,numero,rue,id,mdp);
+        compte.put(id,p); // On l'ajoutes à la HashMap compte
+        // On enregistre les infos dans le fichier texte
+        sauvegarderProfil(nomFichier,p);
+    }
+}
--- src/Model/Particuliers.java
+++ src/Model/Particuliers.java
@@ -12,8 +12,6 @@ public class Particuliers implements Utilisateur
     private boolean estConnecte;
     private Map<String, Profil> compte;
 
-    Scanner sc = new Scanner(System.in);
-
     // CONSTRUCTEUR
     public Particuliers()
     {
@@ -21,89 +19,8 @@ public class Particuliers implements Utilisateur
         this.estConnecte = false;
         this.compte = new HashMap<>();
     }
-    // SETTER n°1
-    public boolean setEstConnecte(boolean estConnecte)
-    {
-        this.estConnecte = estConnecte;
-        return estConnecte;
-    }
 
-    // METHODE n°1 : Login de l'utilisateur
-    @Override
-    public void login()
-    {
-        String choix;
-        System.out.println("Etes-vous déjà inscrit ?");
-        choix = sc.nextLine();
-        switch(choix)
-        {
-            case "oui":
-            { // Connexion (lecture du fichier texte)
-                signin();
-                break;
-            }
-            case "non":
-            { // Se créer un compte (écriture d'un fichier)
-                register();
-                break;
-            }
-            default:{throw new ExceptionPersonnalisable("Choix invalide.");}
-        }
-    }
-
-    // METHODE n°2 : CONNEXION
-    public void signin()
-    {
-        System.out.println("Saisissez votre identifiant:");
-        String idPropose = sc.nextLine(); // On récupère l'identifiant saisi
-        if(compte.containsKey(idPropose)) // Lecture du fichier : l'identifiant existes
-        {
-            Profil p = compte.get(idPropose);
-            System.out.println("Saisissez votre mot de passe:");
-            String mdpPropose = sc.nextLine(); // On récupère le mdp saisi
-            if(p.getMdp().equals(mdpPropose))// On vérifie si c'est le bon mot de passe
-            {
-                setEstConnecte(true);
-                System.out.println("Connexion réussie !");
-            }
-            else
-            {
-                System.out.println("Ce mot de passe est invalide.");
-                signin();
-            }
-        }
-        else
-        {
-            System.out.println("Cet identifiant n'existes pas.");
-            signin();
-        }
-    }
-
-    // METHODE n°3 : INSCRIPTION
-    public void register()
-    {
-        try{}
-        catch(ExceptionPersonnalisable e){}
-        System.out.println("Saisissez votre prenom");
-        String prenom = sc.nextLine();
-        System.out.println("Saisissez votre nom de famille:");
-        String nom = sc.nextLine();
-        System.out.println("Saisissez votre numero d'habitation:");
-        int numero = sc.nextInt();
-        sc.nextLine(); // Vide le buffer
-        System.out.println("Saisissez le nom de votre rue:");
-        String rue = sc.nextLine();
-        String id = UUID.randomUUID().toString(); // Chaque id est différent
-        System.out.println("Voici votre identifiant:" + id);
-        System.out.println("Saisissez un mot de passe:");
-        String mdp = sc.nextLine();
-        // On crée un profil
-        Profil p = new Profil(prenom,nom,numero,rue,id,mdp);
-        compte.put(id,p); // On l'ajoutes à la HashMap compte
-        // On enregistre les infos dans le fichier texte
-        sauvegarderProfil(nomFichier,p);
-    }
-    // METHODE n°4 : Remplissage de la HashMap compte pour la première fois
+    // METHODE n°1 : Remplissage de la HashMap compte pour la première fois
     @Override
     public void chargerInfos(String nomFichier)
     {
@@ -132,7 +49,7 @@ public class Particuliers implements Utilisateur
         System.out.println("Profils chargés avec succès depuis " + nomFichier);
     }
 
-    // METHODE n°5 : lecture après une première ouverture
+    // METHODE n°2 : lecture après une première ouverture
     public void chargerReseauDepuisBuffer()
     {
         try (BufferedReader br = new BufferedReader(new FileReader(nomFichier)))
@@ -166,7 +83,7 @@ public class Particuliers implements Utilisateur
         System.out.println("Profils chargés avec succès depuis " + nomFichier);
     }
 
-    // METHODE n°6 : Sauvegarde dans le fichier texte
+    // METHODE n°3 : Sauvegarde dans le fichier texte
     public void sauvegarderProfil(String nomFichier, Profil p)
     {
         try (BufferedWriter bw = new BufferedWriter(new FileWriter(nomFichier, true)))
@@ -186,14 +103,14 @@ public class Particuliers implements Utilisateur
         }
     }
 
-    // METHODE n°7 : Demander une collecte d'encombrants
+    // METHODE n°4 : Demander une collecte d'encombrants
     @Override
     public void faireDemandeCollecte(String typeUser)
     {
         System.out.println("Voulez-vous demander une collecte d'encombrants");
 
     }
-    // METHODE n°8 : Consulter le planning de collecte (ramassage devant les maisons)
+    // METHODE n°5 : Consulter le planning de collecte (ramassage devant les maisons)
     @Override
     public void consulterPlanningRamassage(String commune) {
 
