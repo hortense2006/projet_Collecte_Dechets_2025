5aa3037783955b12c975e6c6e61f28c3bc61d5ca
valsa <valentine.sauvairemassonnat@edu.ece.fr>
2025-11-14 16:02:28 +0100

    Modification de HO pour une meilleur lecture. Ajout de la lecture dans le main. Création de la boucle qui permet de faire une sortie d'appli.

--- src/Main.java
+++ src/Main.java
@@ -1,61 +1,127 @@
 import Exceptions.ExceptionPersonnalisable;
+import map.Plan;
 
+import java.io.IOException;
 import java.util.Scanner;
 
-public class Main
-{
-    public static void main(String[] args)
-    {
+public class Main {
+
+    public static void main(String[] args) {
+
         // ATTRIBUTS
         String typeUser;
-        int choix;
+        int choixUser;
         Scanner sc = new Scanner(System.in);
         Utilisateur user;
+        Plan plan = new Plan();
+        boolean exit = false;
 
-        System.out.println("Choisissez votre profil:");
-        System.out.println("commune");
-        System.out.println("particulier");
-        System.out.println("entreprise");
-        typeUser = sc.nextLine();
-        switch (typeUser)
-        {
-            case "commune":
-            {
-                System.out.println("Que souhaitez-vous faire :");
-                System.out.println("1. Consulter le plan de Ranville.");
-                System.out.println("2. Mettre à jour les informations de la commune");
+        String nomFichier = "";
+        String choixPlan;
+
+        while (exit != true) {
+
+            System.out.println ("Sur quel type de plan voulez vous vous baser : " +
+                    "\n - HO1 : à bouble sens" +
+                    "\n - HO2 : à sens unique" +
+                    "\n - HO3 : réaliste");
+            choixPlan = sc.nextLine();
+
+            switch (choixPlan) {
+                case "HO1":
+                    nomFichier = "C:\\Users\\valsa\\IdeaProjects\\Théorie des graphes\\projet_Collecte_Dechets_2025\\src\\Ranville_HO1.txt";
+                    try {
+                        plan.chargerPlan(nomFichier, Plan.modeOrientation.HO1_NON_ORIENTE);
+                        System.out.println("Plan de la ville : ");
+                        plan.afficherReseau();
+                        System.out.println("\n");
+                    }
+                    catch (IOException e) {
+                        System.err.println("ERREUR : Impossible de lire le fichier du réseau.");
+                        System.err.println("Détail de l'erreur: " + e.getMessage());
+                        return;
+                    }
+                    break;
+                case "HO2":
+                    nomFichier = "C:\\Users\\valsa\\IdeaProjects\\Théorie des graphes\\projet_Collecte_Dechets_2025\\src\\Ranville_HO2.txt";
+                    try {
+                        plan.chargerPlan(nomFichier, Plan.modeOrientation.HO2_ORIENTE);
+                        System.out.println("Plan de la ville : ");
+                        plan.afficherReseau();
+                        System.out.println("\n");
+                    }
+                    catch (IOException e) {
+                        System.err.println("ERREUR : Impossible de lire le fichier du réseau.");
+                        System.err.println("Détail de l'erreur: " + e.getMessage());
+                        return;
+                    }
+                    break;
+                case "HO3":
+                    nomFichier = "C:\\Users\\valsa\\IdeaProjects\\Théorie des graphes\\projet_Collecte_Dechets_2025\\src\\Ranville_HO3.txt";
+                    try {
+                        plan.chargerPlan(nomFichier, Plan.modeOrientation.HO3_MIXTE);
+                        System.out.println("Plan de la ville : ");
+                        plan.afficherReseau();
+                        System.out.println("\n");
+                    }
+                    catch (IOException e) {
+                        System.err.println("ERREUR : Impossible de lire le fichier du réseau.");
+                        System.err.println("Détail de l'erreur: " + e.getMessage());
+                        return;
+                    }
+                    break;
             }
-            case "particulier":
+
+            System.out.println("Choisissez votre profil : " +
+                    "\n - commune" +
+                    "\n - particulier" +
+                    "\n - entreprise");
+            typeUser = sc.nextLine();
+            switch (typeUser)
             {
-                user = new Particuliers(typeUser);
-                System.out.println("Que souhaitez-vous faire :");
-                System.out.println("1. Connexion");
-                System.out.println("2. Demande de collecte");
-                System.out.println("3. Consulter le planning de ramassage");
-                choix = sc.nextInt();
-                switch (choix)
+                case "commune":
                 {
-                    case 1:
-                    {
-                        user.login();
-                        break;
-                    }
-                    case 2:
-                    {
-                        user.faireDemandeCollecte("particulier");
-                        break;
-                    }
-                    case 3:
-                    {
-                        user.consulterPlanningRamassage("ranville");
-                        break;
-                    }
-                    default: {throw new ExceptionPersonnalisable("Wrong choice");}
+                    System.out.println("Que souhaitez-vous faire :");
+                    System.out.println("1. Consulter le plan de Ranville.");
+                    System.out.println("2. Mettre à jour les informations de la commune");
                 }
+                case "particulier":
+                {
+                    user = new Particuliers(typeUser);
+                    System.out.println("Que souhaitez-vous faire :");
+                    System.out.println("1. Connexion");
+                    System.out.println("2. Demande de collecte");
+                    System.out.println("3. Consulter le planning de ramassage");
+                    System.out.println("4. Quitter");
+                    choixUser = sc.nextInt();
+                    switch (choixUser)
+                    {
+                        case 1:
+                        {
+                            user.login();
+                            break;
+                        }
+                        case 2:
+                        {
+                            user.faireDemandeCollecte("particulier");
+                            break;
+                        }
+                        case 3:
+                        {
+                            user.consulterPlanningRamassage("ranville");
+                            break;
+                        }
+                        case 4:
+                            exit = true;
+                            System.exit(0);// Sortir du programme
+                            break;
+                        default: {throw new ExceptionPersonnalisable("Wrong choice");}
+                    }
 
+                } case "entreprise":{} //je ne pense pas qu'une case entreprise soit necessaire c'est uniquement la commune et un utilisateur qui gére le passage et trajet du camion
+
+                default:{throw new ExceptionPersonnalisable("Wrong choice");}
             }
-            case "entreprise":{}
-            default:{throw new ExceptionPersonnalisable("Wrong choice");}
         }
     }
 }
--- src/map/Plan.java
+++ src/map/Plan.java
@@ -6,41 +6,36 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import Exceptions.ExceptionPersonnalisable;
 
-public class Plan
-{
-    // ATTRIBUTS
+public class Plan {
+
     private Map<String, Station> stations; //plan de la ville
-    // CONSTRUCTEUR
-    public Plan() { //constructeurs
+
+    public Plan() { //constructeur
         this.stations = new HashMap<>();
     }
 
-    public enum ModeOrientation
-    {
+    public enum modeOrientation {
         HO1_NON_ORIENTE, // rue à double sens
         HO2_ORIENTE, // rue à sens unique
         HO3_MIXTE // rue mixte (double sens et sens unique)
     }
 
-    // METHODE n°1
-    private Station CreerStation (String nom)
-    {
-        if (nom.equals("D")) {
+    private Station creerStation (String nom) {
+        if (nom.startsWith("D")) {
             return new Depot(nom);
         }
-        else if (nom.equals("R")) {
+        else if (nom.startsWith("R")) {
             return new Intersection(nom);
         }
-        else if (nom.equals("P")) {
+        else if (nom.startsWith("P")) {
             return new PointCollecte(nom);
         }
         return null;
     }
 
-    // METHODE n°2
-    public void chargerReseau(String nomFichier, ModeOrientation mode) throws IOException
-    {
+    public void chargerPlan (String nomFichier, modeOrientation mode) throws IOException {
         // variable necessaire au chargement et à la création du graphe
         this.stations.clear();
         int nbArcsAjoutes = 0;
@@ -64,45 +59,49 @@ public class Plan
                     nomArrivee = parties[1].trim();
                     try {
                         distance = Double.parseDouble(parties[2].trim());//récupère la distance de la rue
-                    } catch (ExceptionMap e) {
+                    } catch (ExceptionPersonnalisable e) {
                         System.err.println("Ligne ignorée (distance invalide) : " + ligne); //lance une exception si elle n'est pas valide
                         continue;
                     }
                 }
                 else if (parties.length == 4) { // pour HO3
-                    if (!parties[1].trim().equals(">")) {
-                        System.err.println("Ligne ignorée (format invalide) : " + ligne);
+                    nomDepart = parties[0].trim(); //récupère le départ de la rue
+                    String sens = parties[1].trim();
+                    nomArrivee = parties[2].trim(); //récupère la fin de la rue
+                    if (sens.equals(">")) {
+                        estSensUnique = true;
+                    } else if (sens.equals("-")) {
+                        estSensUnique = false;
+                    } else {
+                        System.err.println("Symbole de direction inconnu : " + ligne);
                         continue;
                     }
-                    nomDepart = parties[0].trim(); //récupère le départ de la rue
-                    nomArrivee = parties[2].trim(); //récupère la fin de la rue
-                    estSensUnique = true;
                     try {
-                        distance = Double.parseDouble(parties[2].trim());
-                    } catch (ExceptionMap e) {
+                        distance = Double.parseDouble(parties[3].trim());
+                    } catch (ExceptionPersonnalisable e) {
                         System.err.println("Ligne ignorée (distance invalide) : " + ligne); //lance une exception si elle n'est pas valide
                         continue;
                     }
                 }
                 else {
-                    System.err.println("Ligne ignorée (nombre inférieur à 3 ou supérieur à 4) : " + ligne);
+                    System.err.println("Ligne ignorée (erreur de format) : " + ligne);
                     continue;
                 }
 
-                Station depart = stations.computeIfAbsent(nomDepart, this::CreerStation); // création des sommets
-                Station arrivee =stations.computeIfAbsent(nomArrivee, this::CreerStation);
+                Station depart = stations.computeIfAbsent(nomDepart, this::creerStation); // création des sommets
+                Station arrivee =stations.computeIfAbsent(nomArrivee, this::creerStation);
                 Arc arcAB = new Arc(nomDepart + "-" + nomArrivee, depart, arrivee, distance); // créer un arc type
                 depart.ajouterArcSortant(arcAB); // créer la première connection
                 nbArcsAjoutes++; //incrémente de nom d'arc
 
-                if (mode == ModeOrientation.HO1_NON_ORIENTE) {//initialise le type des graphe
+                if (mode == modeOrientation.HO1_NON_ORIENTE) {//initialise le type des graphe
                     ajouterArcBA = true; //elles sont toutes à double sens
                     typeGraphe = "HO1 (Strictement Double Sens)";
                 }
-                else if (mode == ModeOrientation.HO2_ORIENTE){
+                else if (mode == modeOrientation.HO2_ORIENTE){
                     typeGraphe = "HO2 (Strictement Sens Unique)";
                 }
-                else if  (mode == ModeOrientation.HO3_MIXTE) {
+                else if  (mode == modeOrientation.HO3_MIXTE) {
                     if (!estSensUnique) {
                         ajouterArcBA = true; // C'est une rue double sens
                     }
@@ -120,7 +119,7 @@ public class Plan
         System.out.println("Mode de chargement : " + typeGraphe);
     }
 
-    // METHODE n°3
+
     public void afficherReseau() { //affiche le plan de la ville chargée
         System.out.println("Affichage du plan de la ville");
         for (Station station : stations.values()) {
@@ -130,7 +129,7 @@ public class Plan
             } else {
                 List<String> voisins = new ArrayList<>();
                 for (Arc arc : station.getArcsSortants()) {
-                    voisins.add(arc.getArrivee().getNom() + "(" + (int)arc.getDistance() + ")"); //affiche son voisin
+                    voisins.add(arc.getArrivee().getNom()); //affiche son voisin
                 }
                 System.out.println(String.join(", ", voisins)); //continue
             }
