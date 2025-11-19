576ffb4b7f921e6c6696e1a844385ac41c7bebfd
hortense2006 <hortense.galtier@edu.ece.fr>
2025-11-19 08:42:17 +0100

    Update by Hortense n°101

--- src/model/EntrepriseModel.java
+++ src/model/EntrepriseModel.java
@@ -1,8 +1,14 @@
 package model;
 
 import exceptions.ExceptionPersonnalisable;
+import model.map.Arc;
 import model.map.Plan;
 import model.map.Station;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
 
 public class EntrepriseModel
 {
@@ -15,7 +21,36 @@ public class EntrepriseModel
     }
 
     //METHODE n°1 : Calcul d plus court chemin
-    public void bsfPlusCourtChemin(){}
+    // METHODE n°4 : Calcul du plus court chemin  ( méthode bsf)
+    public Itineraire bfsPlusCourtChemin (String nomDepart, String nomArrivee) throws ExceptionPersonnalisable
+    {
+        Station depart = verifierStations(nomDepart, nomArrivee); // verifie qu'il n'y a pas d'exception
+        Station destination = p.getStation(nomArrivee); // arc par lequel on découvre la station
+        Map<Station, Arc> predecesseurs = new HashMap<>(); //mémoire pour garder toutes les stations passées
+        Queue<Arc> fileArcs = new LinkedList<>();
+        predecesseurs.put(depart, null);
+        fileArcs.addAll(depart.getArcsSortants());
+        Station stationArriveeTrouvee = null;
+        while (!fileArcs.isEmpty() && stationArriveeTrouvee == null)
+        { // tant que le fichier n'est pas vide et qu'on est pas arrivé
+            Arc arcCourant = fileArcs.poll(); //initialise l'arc sur lequel on est
+            Station successeur = arcCourant.getArrivee(); // regarde la station qui arrive
+            if (!predecesseurs.containsKey(successeur))
+            { //pour ne pas tourner en rond
+                predecesseurs.put(successeur, arcCourant); //met en mémoir l'arc qu'on vient de fair et la station d'avant
+                if (successeur.equals(destination))
+                { // arrête si l'arrivé est trouver
+                    stationArriveeTrouvee = successeur;
+                }
+                else
+                {
+                    fileArcs.addAll(successeur.getArcsSortants()); //sinon on continue
+                }
+            }
+        }
+        return reconstituerChemin(depart, destination, stationArriveeTrouvee, predecesseurs);//pour afficher grace à la méthode dans afficher itinéraire
+    }
+
 
     // METHODE n°2 : Vérification des stations
     public Station verifierStations(String nomDepart, String nomArrivee) throws ExceptionPersonnalisable
