e057a2fd5d0eb55e617c9b5f34e33efce06de98a
valsa <valentine.sauvairemassonnat@edu.ece.fr>
2025-11-25 15:32:20 +0100

    5

--- src/controller/PlanController.java
+++ src/controller/PlanController.java
@@ -25,14 +25,10 @@ public class PlanController {
 
             switch (choix) { //applique le type de plan que nous utilisons
                 case 1:
-                    nomFichier = "C:\\Users\\valsa\\IdeaProjects\\Théorie des graphes\\projet_Collecte_Dechets_2025\\src\\Ranville_HO1.txt";
-                    //c'est un absolute path tu peux le changer pour qu'il fonctionne sur ton PC si tu veux
-                    // suffit de clic droit sur le fichier HO, et faire get absolute path et encore get absolut path et coller
-                    //merci de n'utiliser aucune autre forme que tu absolute path
+                    nomFichier = "Ranville_HO1.txt";
 
                     try {
                         p.chargerPlan(nomFichier, Plan.modeOrientation.HO1_NON_ORIENTE);
-                        System.out.println("Plan de la ville : ");
                         planView.afficherMessagePlan("\n");
                         return p;
                     } catch (IOException e) { // lance l'erreur necessaire
@@ -41,10 +37,9 @@ public class PlanController {
                     }
                     break;
                 case 2:
-                    nomFichier = "C:\\Users\\valsa\\IdeaProjects\\Théorie des graphes\\projet_Collecte_Dechets_2025\\src\\Ranville_HO2.txt";
+                    nomFichier = "Ranville_HO2.txt";
                     try {
                         p.chargerPlan(nomFichier, Plan.modeOrientation.HO2_ORIENTE);
-                        System.out.println("Plan de la ville : ");
                         planView.afficherMessagePlan("\n");
                         return p;
                     } catch (IOException e) {
@@ -53,10 +48,9 @@ public class PlanController {
                     }
                     break;
                 case 3:
-                    nomFichier = "C:\\Users\\valsa\\IdeaProjects\\Théorie des graphes\\projet_Collecte_Dechets_2025\\src\\Ranville_HO3.txt";
+                    nomFichier = "Ranville_HO3.txt";
                     try {
                         plan.chargerPlan(nomFichier, Plan.modeOrientation.HO3_MIXTE);
-                        System.out.println("Plan de la ville : ");
                         planView.afficherMessagePlan("\n");
                         return p;
                     } catch (IOException e) {
