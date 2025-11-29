b15aa5a56d43b021905b77fd165bfb47fbb8b382
valsa <valentine.sauvairemassonnat@edu.ece.fr>
2025-11-29 19:07:43 +0100

    18

--- src/view/PointCollecteView.java
+++ src/view/PointCollecteView.java
@@ -1,5 +1,9 @@
 package view;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class PointCollecteView {
@@ -15,12 +19,10 @@ public class PointCollecteView {
         System.out.println(message);
     }
 
-
     public String demanderNomPoint() { // Demande à l'habitant le nom du point
         System.out.println("Entrez le nom du point de collecte oùu vous allez jeter vos déchets : ");
         return sc.nextLine().trim();
     }
-
 
     public int demanderQuantite() { // Demande à l'habitant la quantité
         System.out.println ("Entrez la quantité de déchet que vous allez jeter : ");
@@ -30,4 +32,34 @@ public class PointCollecteView {
             return -1; // Code d'erreur si ce n'est pas un nombre
         }
     }
+
+    public static void afficherEtatPointCollecte() {
+        File file = new File("Etat_point_de_collecte.txt");
+
+        if (!file.exists()) {
+            System.err.println("Le fichier n'existe pas");
+            return;
+        }
+
+        System.out.println("\n Etat des points de collecte");
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            String ligne;
+            boolean estVide = true;
+            while ((ligne = reader.readLine()) != null) {
+                String[] parts = ligne.split(";");
+                if (parts.length == 2) {
+                    String nom = parts[0];
+                    String quantite = parts[1];
+                    System.out.println(" - Point " + nom + " : " + quantite + " déchets stockés");
+                    estVide = false;
+                }
+            }
+            if (estVide) {
+                System.out.println("Le fichier est vide.");
+            }
+        } catch (IOException e) {
+            System.err.println("Erreur lors de la lecture du fichier : " + e.getMessage());
+        }
+    }
+
 }
