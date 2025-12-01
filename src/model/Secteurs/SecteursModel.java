c16d5780625c3cc32362d8a12077f6a2781fcc06
hortense2006 <hortense.galtier@edu.ece.fr>
2025-12-01 14:45:49 +0100

    Update by Hortense n°237

--- src/model/Secteurs/SecteursModel.java
+++ src/model/Secteurs/SecteursModel.java
@@ -2,48 +2,94 @@ package model.Secteurs;
 
 import exceptions.ExceptionPersonnalisable;
 
-import java.util.Comparator;
-import java.util.List;
+import java.io.IOException;
+import java.util.*;
 
 public class SecteursModel implements Comparator<Secteurs>
 {
+    //ATTRIBUTS
+    private FichierSecteurs fichiers;
+    HashMap<String, Secteurs> secteur;
+    HashMap<Integer, Secteurs> couleurParSecteurs;
+    // CONSTRUCTEUR
+    public SecteursModel(FichierSecteurs fichiers)
+    {
+        this.fichiers = fichiers;
+        secteur = fichiers.getSecteurs();
+        couleurParSecteurs = new HashMap<>();
+    }
     // METHODE n°1 : Comparer deux secteurs (incompatibilité selon critères géographiques + jours de passage)
     @Override
     public int compare(Secteurs o1, Secteurs o2)
     {
-        int degre1; // 0 si c'est incompatible, 1 si c'est compatible
-        try
+        // On récupère les sommets du secteur o1 sous forme de liste
+        List<String> s1 = Arrays.asList(o1.getSommets().split(","));
+        // On récupère les sommets du secteur o2 sous forme de liste
+        List<String> s2 = Arrays.asList(o2.getSommets().split(","));
+
+        // Deux secteurs sont incompatibles s'ils ont au moins 1 sommet en commun
+        boolean incompatibles = s1.stream().anyMatch(s2::contains);
+
+        if (incompatibles)
         {
-            degre1 = getIncompatibilitesSecteurs(o1,o2);
+            // Les secteurs sont incompatibles (voisins)
+            return 0;
         }
-        catch (Exception e)
+        else
         {
-            throw new ExceptionPersonnalisable("Produit non trouve.");
+            // compatibles (ne sont pas voisins)
+            return Integer.compare(s1.size(), s2.size());
         }
-        int degre2 = getIncompatibilitesSecteurs(o1,o2);
-        return Integer.compare(degre1,degre2);
     }
     // METHODE n°2 : Incompatilibités des secteurs
     public int getIncompatibilitesSecteurs(Secteurs o1,Secteurs o2)
-    {return 0;}
+    {
+        //Deux secteurs sont incompatibles s'ils ont  au moins 1 sommet en commun
+        // On compare deux somme
+        return 0;
+    }
     // METHODE 3 : Remplissage des couleurs
-    public int welshPowell()
-    { /*List<Secteurs> couleur;
-        Secteurs color;
-        String nbzones;
-        // Lire la liste produits
-        while(!couleur.isEmpty()) // On lit la case de la liste
+    // Renvoie le nombre de couleur
+    // 1 couleur/secteur
+    public int welshPowell() throws IOException
+    {
+        // On charge le graphe des secteurs
+        fichiers.chargerDepuisFichier();
+        // On récupère les secteurs
+        // Chaque secteur a un nom, une couleur, un point de collecte,des sommets & des arcs
+        HashMap<Secteurs, Set<Secteurs>> graphe = new HashMap<>();
+        // On construit le graphe d'adjacence
+        for(Secteurs s : secteur.values())
         {
-            for (Secteurs p : couleur)
+            graphe.put(s, new HashSet<>());
+            for (Secteurs s2 : secteur.values())
             {
-                // Si le produit n'a pas encore de zone
-                if (p.getCouleur() == 0)
+                if (compare(s, s2) > 0) // Si les secteurs ne sont pas voisins
                 {
-                    nbzones++;
-                    p.setCouleur(nbzones); // on lui attribue une nouvelle zone
+                    graphe.get(s).add(s2); // On ajoute au graphe
                 }
             }
-        }*/
-        return 1;
+        }
+        // On trie les secteurs
+        triSecteursPardegre();
+        // Welsh-Powell
+        Secteurs color;
+        int nbzones = 0;
+        // Lire la liste produits
+        while(!couleurParSecteurs.isEmpty()) // On lit la case de la HashMap
+        {
+            for (Secteurs s : color)
+            {
+                // Si le produit n'a pas encore de zone
+                if (s.getCouleur() == null)
+                {
+                    nbzones++;
+                    s.setCouleur(nbzones); // on lui attribue une nouvelle zone
+                }
+            }
+        }
+        return nbzones;
     }
+    // METHODE n°4 : Tri de la HashMap
+    public void triSecteursPardegre(){}
 }
