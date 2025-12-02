6b92ccbd68b533ba9e292c190bbec04d01692c7d
hortense2006 <hortense.galtier@edu.ece.fr>
2025-12-02 18:29:00 +0100

    Update by Hortense n°238

--- src/model/Secteurs/SecteursModel.java
+++ src/model/Secteurs/SecteursModel.java
@@ -1,95 +1,103 @@
 package model.Secteurs;
-
-import exceptions.ExceptionPersonnalisable;
-
 import java.io.IOException;
 import java.util.*;
 
-public class SecteursModel implements Comparator<Secteurs>
+public class SecteursModel
 {
     //ATTRIBUTS
     private FichierSecteurs fichiers;
     HashMap<String, Secteurs> secteur;
-    HashMap<Integer, Secteurs> couleurParSecteurs;
+    List<Secteurs> secteursIncompatibles;
     // CONSTRUCTEUR
     public SecteursModel(FichierSecteurs fichiers)
     {
         this.fichiers = fichiers;
         secteur = fichiers.getSecteurs();
-        couleurParSecteurs = new HashMap<>();
+        secteursIncompatibles = new ArrayList<>();
     }
     // METHODE n°1 : Comparer deux secteurs (incompatibilité selon critères géographiques + jours de passage)
-    @Override
-    public int compare(Secteurs o1, Secteurs o2)
-    {
-        // On récupère les sommets du secteur o1 sous forme de liste
-        List<String> s1 = Arrays.asList(o1.getSommets().split(","));
-        // On récupère les sommets du secteur o2 sous forme de liste
-        List<String> s2 = Arrays.asList(o2.getSommets().split(","));
 
-        // Deux secteurs sont incompatibles s'ils ont au moins 1 sommet en commun
-        boolean incompatibles = s1.stream().anyMatch(s2::contains);
-
-        if (incompatibles)
-        {
-            // Les secteurs sont incompatibles (voisins)
-            return 0;
-        }
-        else
-        {
-            // compatibles (ne sont pas voisins)
-            return Integer.compare(s1.size(), s2.size());
-        }
-    }
-    // METHODE n°2 : Incompatilibités des secteurs
-    public int getIncompatibilitesSecteurs(Secteurs o1,Secteurs o2)
+    public List<Secteurs> comparerSecteurs(Secteurs o1, Secteurs o2)
     {
-        //Deux secteurs sont incompatibles s'ils ont  au moins 1 sommet en commun
-        // On compare deux somme
-        return 0;
+    // On récupère les sommets du secteur o1 sous forme de liste
+    List<String> s1 = Arrays.asList(o1.getSommets().split(","));
+    // On récupère les sommets du secteur o2 sous forme de liste
+    List<String> s2 = Arrays.asList(o2.getSommets().split(","));
+
+    // Deux secteurs sont incompatibles s'ils ont au moins 1 sommet en commun
+    boolean incompatibles = s1.stream().anyMatch(s2::contains);
+
+    if (incompatibles)
+    {
+        // Les secteurs sont incompatibles (voisins)
+        Secteurs sfusionne = fusionnerSecteurs(o1, o2);
+        secteursIncompatibles.add(sfusionne);// On met le couple de secteurs compatibles dans une hashmap.
+        return secteursIncompatibles;
     }
-    // METHODE 3 : Remplissage des couleurs
-    // Renvoie le nombre de couleur
+    // Sinon : compatibles (ne sont pas voisins)
+        return null;
+    }
+
+    // METHODE 2: Fusion des secteurs
+    public Secteurs fusionnerSecteurs(Secteurs s1, Secteurs s2)
+    {
+
+        // Nouveau nom pour le secteur fusionné
+        String nouveauNom = s1.getNom() + "_" + s2.getNom();
+
+        // Fusion des sommets (évite les doublons)
+        Set<String> nouveauxSommets = new HashSet<>(Collections.singleton(s1.getSommets()));
+        nouveauxSommets.addAll(Collections.singleton(s2.getSommets()));
+
+        // Fusion des arcs
+        Set<String> nouveauxArcs = new HashSet<>(Collections.singleton(s1.getArcAssocie()));
+        nouveauxArcs.addAll(Collections.singleton(s2.getArcAssocie()));
+
+        // Créer un nouveau secteur fusionné
+        Secteurs fusion = new Secteurs(
+                nouveauNom,"BLANC",  // couleur par défaut, car on ne l'as pas encore calculée
+                nouveauxSommets.toString(),
+                nouveauxArcs.toString()
+        );
+
+        return fusion;
+    }
+
+    //METHODE n°3 :  Renvoie le nombre de couleurs (le nombre de secteur
     // 1 couleur/secteur
-    public int welshPowell() throws IOException
+    public void welshPowell() throws IOException
     {
         // On charge le graphe des secteurs
         fichiers.chargerDepuisFichier();
-        // On récupère les secteurs
+        // On récupère la liste des secteurs incompatibles
+        // secteursIncompatibles = graphe d'adjacence
         // Chaque secteur a un nom, une couleur, un point de collecte,des sommets & des arcs
-        HashMap<Secteurs, Set<Secteurs>> graphe = new HashMap<>();
-        // On construit le graphe d'adjacence
-        for(Secteurs s : secteur.values())
+        for(Secteurs o1 : secteur.values())
         {
-            graphe.put(s, new HashSet<>());
-            for (Secteurs s2 : secteur.values())
+            for(Secteurs o2 : secteur.values())
             {
-                if (compare(s, s2) > 0) // Si les secteurs ne sont pas voisins
-                {
-                    graphe.get(s).add(s2); // On ajoute au graphe
-                }
+                secteursIncompatibles = comparerSecteurs(o1,o2);
             }
         }
         // On trie les secteurs
-        triSecteursPardegre();
+        triSecteursPardegre(secteursIncompatibles);
         // Welsh-Powell
         Secteurs color;
         int nbzones = 0;
         // Lire la liste produits
-        while(!couleurParSecteurs.isEmpty()) // On lit la case de la HashMap
+        while(!secteursIncompatibles.isEmpty()) // On lit la case de la liste
         {
-            for (Secteurs s : color)
+            for (Secteurs s : secteursIncompatibles)
             {
                 // Si le produit n'a pas encore de zone
                 if (s.getCouleur() == null)
                 {
                     nbzones++;
-                    s.setCouleur(nbzones); // on lui attribue une nouvelle zone
+                    s.setCouleur(String.valueOf(nbzones)); // on lui attribue une nouvelle zone
                 }
             }
         }
-        return nbzones;
     }
     // METHODE n°4 : Tri de la HashMap
-    public void triSecteursPardegre(){}
+    public void triSecteursPardegre(List<Secteurs> secteursIncompatibles){}
 }
