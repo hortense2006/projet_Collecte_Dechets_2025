3c1a13413ae02e032bd35538f3f1a5992c1e86f6
hortense2006 <hortense.galtier@edu.ece.fr>
2025-12-04 12:06:15 +0100

    Update by Hortense n°255

--- src/model/particulier/ProfilInput.java
+++ src/model/particulier/ProfilInput.java
@@ -1,4 +1,4 @@
 package model.particulier;
 
 // DT0 (Data Transfer Object)
-public record ProfilInput(String prenom, String nom, double numero, String rue, String mdp) {}
+public record ProfilInput(String nomVille,String prenom, String nom, double numero, String rue, String mdp) {}
